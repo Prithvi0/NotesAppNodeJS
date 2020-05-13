ac9c740abdf5e1a761714653ef8f11f419c3c3bd
Prithvi0 <prithviy96@gmail.com>
2020-05-13 21:59:20 +0530

    Modified server file to connect to database using mongoose

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 
 // create an app using express
 const app = express();
@@ -10,12 +14,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse request of content-type: application/json
 app.use(bodyParser.json());
 
+// connecting to the database
+mongoose.connect(dbConfig.url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Successfully connected to the database');
+}).catch(error => {
+    console.log('Could not connect to the database. Exiting now...', error);
+    process.exit();
+});
+
 // defining a route
 app.get('/', (request, response) => {
-    response.json({'message': 'Welcome to Notes application!'});
+    response.json({ 'message': 'Welcome to Notes application!' });
 });
 
 // listen for requests
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000'); 
+    console.log('Server is listening on port 3000');
 });
